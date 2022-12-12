6ec329cf9805e3024d23062349ab4aa0d70cc509
Jamesson Junior <jamesson.junior@capco.com>
2022-12-12 12:03:53 -0300

    Configuring Eureka Clients

--- hr-payroll/src/main/java/br/com/jamesson/hrpayrool/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/jamesson/hrpayrool/services/PaymentService.java
@@ -3,13 +3,8 @@ package br.com.jamesson.hrpayrool.services;
 import br.com.jamesson.hrpayrool.entities.Payment;
 import br.com.jamesson.hrpayrool.entities.Worker;
 import br.com.jamesson.hrpayrool.feignClients.WorkerFeignClient;
-import java.net.URI;
-import java.util.HashMap;
-import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class PaymentService {
