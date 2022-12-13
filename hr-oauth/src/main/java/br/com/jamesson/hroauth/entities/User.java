027c83480505db5ba072a9c0be60b545c8bcfdd4
Jamesson Junior <jamesson.junior@capco.com>
2022-12-13 11:29:31 -0300

    Login, JWT Token

--- hr-oauth/src/main/java/br/com/jamesson/hroauth/entities/User.java
+++ hr-oauth/src/main/java/br/com/jamesson/hroauth/entities/User.java
@@ -1,15 +1,18 @@
 package br.com.jamesson.hroauth.entities;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-public class User implements Serializable {
-
+public class User implements UserDetails, Serializable {
 
   private Long id;
   private String name;
-
   private String email;
   private String password;
 
@@ -49,16 +52,47 @@ public class User implements Serializable {
     this.email = email;
   }
 
-  public String getPassword() {
-    return password;
-  }
-
   public void setPassword(String password) {
     this.password = password;
   }
 
+  public String getPassword() {
+    return password;
+  }
+
   public Set<Role> getRoles() {
     return roles;
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return roles.stream().map(x -> new SimpleGrantedAuthority(x.getRoleName()))
+        .collect(Collectors.toList());
+  }
+
+  @Override
+  public String getUsername() {
+    return email;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
   }
 
   @Override
